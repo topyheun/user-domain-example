daabf34ae5db7f3ac20c593c079080db33abdd39
Topy <gmsdl1994@naver.com>
2023-05-23 15:12:05 +0900

    create project
    #1

---
+++ src/main/java/topy/project/TopyApplication.java
@@ -0,0 +1,13 @@
+package topy.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/topy/project/TopyApplicationTests.java
@@ -0,0 +1,13 @@
+package topy.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
