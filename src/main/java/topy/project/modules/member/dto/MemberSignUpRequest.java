f8ad054136fb10baf1d0fff00c387d593115d4ec
Topy <gmsdl1994@naver.com>
2023-05-24 16:22:52 +0900

    feat: Modify valid message using const

--- src/main/java/topy/project/modules/member/dto/MemberSignUpRequest.java
+++ src/main/java/topy/project/modules/member/dto/MemberSignUpRequest.java
@@ -4,13 +4,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
+import static topy.project.common.Const.*;
+
 @Getter
 public class MemberSignUpRequest {
 
-    @NotBlank(message = "아이디를 입력해주세요.")
+    @NotBlank(message = MEMBER_DTO_NO_USERNAME)
     private String username;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_\\-+=~`\\[\\]{}|:;\"'<>,.?\\\\/])[A-Za-z\\d!@#$%^&*()_\\-+=~`\\[\\]{}|:;\"'<>,.?\\\\/]{8,20}$", message = "숫자, 대문자, 소문자, 특수문자를 포함하여 8 ~ 20자리의 비밀번호를 입력해주세요.")
+    @NotBlank(message = MEMBER_DTO_NO_PASSWORD)
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_\\-+=~`\\[\\]{}|:;\"'<>,.?\\\\/])[A-Za-z\\d!@#$%^&*()_\\-+=~`\\[\\]{}|:;\"'<>,.?\\\\/]{8,20}$", message = MEMBER_DTO_WRONG_PASSWORD_RULE)
     private String password;
 }
