b398da4b3a8e6f983c0b661e55abdf6e5f676cc0
Topy <gmsdl1994@naver.com>
2023-05-24 16:20:44 +0900

    feat: Add const

---
+++ src/main/java/topy/project/common/Const.java
@@ -0,0 +1,18 @@
+package topy.project.common;
+
+public class Const {
+
+    /**
+     * 공통
+     */
+    public static final int SUCCESS = 0;
+    public static final String SUCCESS_MSG = "성공";
+
+    /**
+     * 명명 규칙
+     * Domain을 접두사로 사용
+     */
+    public static final String MEMBER_DTO_NO_USERNAME = "아이디를 입력해주세요.";
+    public static final String MEMBER_DTO_NO_PASSWORD = "비밀번호를 입력해주세요.";
+    public static final String MEMBER_DTO_WRONG_PASSWORD_RULE = "숫자, 대문자, 소문자, 특수문자를 포함하여 8 ~ 20자리의 비밀번호를 입력해주세요.";
+}
